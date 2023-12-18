72c68d99807e4225a039367bb51cd895278f88ad
Byeonghak Kwon <kbh88zz@kdiwin.com>
2023-12-18 19:49:29 +0900

    Implemented the control view of power saver

--- app/src/main/java/kr/or/kashi/hde/device/PowerSaverView.java
+++ app/src/main/java/kr/or/kashi/hde/device/PowerSaverView.java
@@ -19,25 +19,104 @@ package kr.or.kashi.hde.device;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import kr.or.kashi.hde.R;
+import kr.or.kashi.hde.base.PropertyMap;
+import kr.or.kashi.hde.base.PropertyValue;
 import kr.or.kashi.hde.widget.HomeDeviceView;
 
-public class PowerSaverView extends HomeDeviceView<PowerSaver> {
+public class PowerSaverView extends HomeDeviceView<PowerSaver> implements View.OnClickListener {
     private static final String TAG = PowerSaverView.class.getSimpleName();
     private final Context mContext;
+
+    private CheckBox mStateCheck;
+    private CheckBox mOverloadDetectedCheck;
+    private CheckBox mStandbyDetectedCheck;
+    private CheckBox mSettingCheck;
+    private RadioButton mStandbyBlockingOffRadio;
+    private RadioButton mStandbyBlockingOnRadio;
+    private CheckBox mCurrentPowerCheck;
+    private TextView mCurrentPowerText;
+    private CheckBox mStandbyPowerCheck;
+    private TextView mStandbyPowerText;
+    private EditText mStandbyPowerEdit;
+    private Button mStandbyPowerSetButton;
 
     public PowerSaverView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
-
     }
 
     @Override
     protected void onFinishInflate () {
         super.onFinishInflate();
 
+        mStateCheck = findViewById(R.id.state_check);
+        mStateCheck.setEnabled(true);
+        mStateCheck.setClickable(false);
+        mOverloadDetectedCheck = findViewById(R.id.overload_detected_check);
+        mOverloadDetectedCheck.setClickable(false);
+        mStandbyDetectedCheck = findViewById(R.id.standby_detected_check);
+        mStandbyDetectedCheck.setClickable(false);
+        mSettingCheck = findViewById(R.id.setting_check);
+        mStandbyBlockingOffRadio = findViewById(R.id.standby_blocking_off_radio);
+        mStandbyBlockingOffRadio.setOnClickListener(this);
+        mStandbyBlockingOnRadio = findViewById(R.id.standby_blocking_on_radio);
+        mStandbyBlockingOnRadio.setOnClickListener(this);
+        mCurrentPowerCheck = findViewById(R.id.current_power_check);
+        mCurrentPowerText = findViewById(R.id.current_power_text);
+        mStandbyPowerCheck = findViewById(R.id.standby_power_check);
+        mStandbyPowerText = findViewById(R.id.standby_power_text);
+        mStandbyPowerEdit = findViewById(R.id.standby_power_edit);
+        mStandbyPowerSetButton = findViewById(R.id.standby_power_set_button);
+        mStandbyPowerSetButton.setOnClickListener(this);
     }
 
+    @Override
+    public void onUpdateProperty(PropertyMap props, PropertyMap changed) {
+        final long supportedStates = props.get(PowerSaver.PROP_SUPPORTED_STATES, Long.class);
+        mOverloadDetectedCheck.setEnabled((supportedStates & PowerSaver.State.OVERLOAD_DETECTED) != 0);
+        mStandbyDetectedCheck.setChecked((supportedStates & PowerSaver.State.STANDBY_DETECTED) != 0);
+
+        final long currentStates = props.get(PowerSaver.PROP_CURRENT_STATES, Long.class);
+        mOverloadDetectedCheck.setChecked((currentStates & PowerSaver.State.OVERLOAD_DETECTED) != 0);
+        mStandbyDetectedCheck.setChecked((currentStates & PowerSaver.State.STANDBY_DETECTED) != 0);
+
+        final long supportedSettings = props.get(PowerSaver.PROP_SUPPORTED_SETTINGS, Long.class);
+        mStandbyBlockingOffRadio.setEnabled((supportedSettings & PowerSaver.Setting.STANDBY_BLOCKING_ON) != 0);
+        mStandbyBlockingOnRadio.setEnabled((supportedSettings & PowerSaver.Setting.STANDBY_BLOCKING_ON) != 0);
+
+        final long currentSettings = props.get(PowerSaver.PROP_CURRENT_SETTINGS, Long.class);
+        mStandbyBlockingOffRadio.setChecked((currentSettings & PowerSaver.Setting.STANDBY_BLOCKING_ON) == 0);
+        mStandbyBlockingOnRadio.setChecked((currentSettings & PowerSaver.Setting.STANDBY_BLOCKING_ON) != 0);
+
+        final float currentConsumption = props.get(PowerSaver.PROP_CURRENT_CONSUMPTION, Float.class);
+        mCurrentPowerText.setText("" + currentConsumption);
+
+        final float standbyConsumption = props.get(PowerSaver.PROP_STANDBY_CONSUMPTION, Float.class);
+        mStandbyPowerText.setText("" + standbyConsumption);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mStandbyBlockingOffRadio) {
+            final long currentSettings = device().getProperty(PowerSaver.PROP_CURRENT_SETTINGS, Long.class);
+            device().setProperty(PowerSaver.PROP_CURRENT_SETTINGS, Long.class, currentSettings & ~PowerSaver.Setting.STANDBY_BLOCKING_ON);
+        } else if (v == mStandbyBlockingOnRadio) {
+            final long currentSettings = device().getProperty(PowerSaver.PROP_CURRENT_SETTINGS, Long.class);
+            device().setProperty(PowerSaver.PROP_CURRENT_SETTINGS, Long.class, currentSettings | PowerSaver.Setting.STANDBY_BLOCKING_ON);
+        } else if (v == mStandbyPowerSetButton) {
+            final String editStr = mStandbyPowerEdit.getText().toString();
+            float standbyConsumption = PropertyValue.newValueObject(Float.class, editStr);
+            device().setProperty(PowerSaver.PROP_STANDBY_CONSUMPTION, Float.class, standbyConsumption);
+        }
+    }
 }
