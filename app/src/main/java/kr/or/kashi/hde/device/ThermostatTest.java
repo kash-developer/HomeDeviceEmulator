11421f08c4d48f7d933440b9d9f13103b6266372
Byeonghak Kwon <kbh88zz@kdiwin.com>
2023-12-19 11:01:51 +0900

    Implemented the control view and test cases for the thermostat device

---
+++ app/src/main/java/kr/or/kashi/hde/device/ThermostatTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2023 Korea Association of AI Smart Home.
+ * Copyright (C) 2023 KyungDong Navien Co, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kr.or.kashi.hde.device;
+
+import kr.or.kashi.hde.test.DeviceTestCase;
+
+public class ThermostatTest extends DeviceTestCase {
+    public void test_Heating() throws Exception {
+        doFunctionTest(Thermostat.Function.HEATING);
+    }
+
+    public void test_OutingSetting() throws Exception {
+        doFunctionTest(Thermostat.Function.OUTING_SETTING);
+    }
+
+    public void test_ReservedMode() throws Exception {
+        doFunctionTest(Thermostat.Function.RESERVED_MODE);
+    }
+
+    private void doFunctionTest(long testFunction) throws Exception {
+        assertSupported(Thermostat.PROP_SUPPORTED_FUNCTIONS, testFunction);
+
+        long functionStates = device().getProperty(Thermostat.PROP_FUNCTION_STATES, Long.class);
+        device().setProperty(Thermostat.PROP_FUNCTION_STATES, Long.class, (functionStates | testFunction));
+        waitFor(2000);
+        functionStates = device().getProperty(Thermostat.PROP_FUNCTION_STATES, Long.class);
+        assertTrue((functionStates & testFunction) != 0);
+    }
+
+    public void test_TemperatureSetting() throws Exception {
+        final float minTemp = device().getProperty(Thermostat.PROP_MIN_TEMPERATURE, Float.class);
+        final float maxTemp = device().getProperty(Thermostat.PROP_MAX_TEMPERATURE, Float.class);
+        final float tempRes = device().getProperty(Thermostat.PROP_TEMP_RESOLUTION, Float.class);
+        final float reqTemp = minTemp + tempRes;
+
+        assertTrue(reqTemp < maxTemp);
+
+        device().setProperty(Thermostat.PROP_SETTING_TEMPERATURE, Float.class, minTemp);
+        waitFor(2000);
+        device().setProperty(Thermostat.PROP_SETTING_TEMPERATURE, Float.class, reqTemp);
+        waitFor(2000);
+        assertEquals("", reqTemp, device().getProperty(Thermostat.PROP_SETTING_TEMPERATURE, Float.class), 0.005f /* epsilon */);
+        // NOTE: Checks only request temperature because the actual temperature changes slowly depending on the target device
+    }
+}
--- app/src/main/java/kr/or/kashi/hde/device/ThermostatView.java
+++ app/src/main/java/kr/or/kashi/hde/device/ThermostatView.java
@@ -19,25 +19,112 @@ package kr.or.kashi.hde.device;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import kr.or.kashi.hde.R;
+import kr.or.kashi.hde.base.PropertyMap;
+import kr.or.kashi.hde.base.PropertyValue;
 import kr.or.kashi.hde.widget.HomeDeviceView;
 
 public class ThermostatView extends HomeDeviceView<Thermostat> {
     private static final String TAG = ThermostatView.class.getSimpleName();
     private final Context mContext;
 
+    private CheckBox mFunctionCheck;
+    private CheckBox mHeatingCheck;
+    private CheckBox mCoolingCheck;
+    private CheckBox mOutingSettingCheck;
+    private CheckBox mHotwaterOnlyCheck;
+    private CheckBox mReservedModeCheck;
+    private CheckBox mTempRangeCheck;
+    private TextView mTempRangeText;
+    private CheckBox mCurrentTempCheck;
+    private TextView mCurrentTempText;
+    private CheckBox mSettingTempCheck;
+    private TextView mSettingTempText;
+    private EditText mSettingTempEdit;
+    private Button mTempSettingButton;
+
     public ThermostatView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         mContext = context;
-
     }
 
     @Override
     protected void onFinishInflate () {
         super.onFinishInflate();
 
+        mFunctionCheck = findViewById(R.id.function_check);
+        mHeatingCheck = findViewById(R.id.function_heating_check);
+        mHeatingCheck.setOnClickListener(v -> setFunctions());
+        mCoolingCheck = findViewById(R.id.function_cooling_check);
+        mCoolingCheck.setOnClickListener(v -> setFunctions());
+        mOutingSettingCheck = findViewById(R.id.function_outing_setting_check);
+        mOutingSettingCheck.setOnClickListener(v -> setFunctions());
+        mHotwaterOnlyCheck = findViewById(R.id.function_hotwater_only_check);
+        mHotwaterOnlyCheck.setOnClickListener(v -> setFunctions());
+        mReservedModeCheck = findViewById(R.id.function_reserved_mode_check);
+        mReservedModeCheck.setOnClickListener(v -> setFunctions());
+        mTempRangeCheck = findViewById(R.id.temp_range_check);
+        mTempRangeText = findViewById(R.id.temp_range_text);
+        mCurrentTempCheck = findViewById(R.id.current_temp_check);
+        mCurrentTempText = findViewById(R.id.current_temp_text);
+        mSettingTempCheck = findViewById(R.id.setting_temp_check);
+        mSettingTempText = findViewById(R.id.setting_temp_text);
+        mSettingTempEdit = findViewById(R.id.setting_temp_edit);
+        mTempSettingButton = findViewById(R.id.temp_setting_button);
+        mTempSettingButton.setOnClickListener(v -> setTemperature());
     }
 
+    @Override
+    public void onUpdateProperty(PropertyMap props, PropertyMap changed) {
+        final long supportedFunctions = props.get(Thermostat.PROP_SUPPORTED_FUNCTIONS, Long.class);
+        mHeatingCheck.setEnabled((supportedFunctions & Thermostat.Function.HEATING) != 0);
+        mCoolingCheck.setEnabled((supportedFunctions & Thermostat.Function.COOLING) != 0);
+        mOutingSettingCheck.setEnabled((supportedFunctions & Thermostat.Function.OUTING_SETTING) != 0);
+        mHotwaterOnlyCheck.setEnabled((supportedFunctions & Thermostat.Function.HOTWATER_ONLY) != 0);
+        mReservedModeCheck.setEnabled((supportedFunctions & Thermostat.Function.RESERVED_MODE) != 0);
+
+        final long functionStates = props.get(Thermostat.PROP_FUNCTION_STATES, Long.class);
+        mHeatingCheck.setChecked((functionStates & Thermostat.Function.HEATING) != 0);
+        mCoolingCheck.setChecked((functionStates & Thermostat.Function.COOLING) != 0);
+        mOutingSettingCheck.setChecked((functionStates & Thermostat.Function.OUTING_SETTING) != 0);
+        mHotwaterOnlyCheck.setChecked((functionStates & Thermostat.Function.HOTWATER_ONLY) != 0);
+        mReservedModeCheck.setChecked((functionStates & Thermostat.Function.RESERVED_MODE) != 0);
+
+        final float minTemp = props.get(Thermostat.PROP_MIN_TEMPERATURE, Float.class);
+        final float maxTemp = props.get(Thermostat.PROP_MAX_TEMPERATURE, Float.class);
+        final float tempRes = props.get(Thermostat.PROP_TEMP_RESOLUTION, Float.class);
+        mTempRangeText.setText("min:" + minTemp + ", max:" + maxTemp + ", res:" + tempRes);
+
+        final float curTemp = props.get(Thermostat.PROP_CURRENT_TEMPERATURE, Float.class);
+        mCurrentTempText.setText("" + curTemp);
+
+        final float setTemp = props.get(Thermostat.PROP_SETTING_TEMPERATURE, Float.class);
+        mSettingTempText.setText("" + setTemp);
+        if (!mSettingTempEdit.hasFocus()) mSettingTempEdit.setText("" + setTemp);
+    }
+
+    private void setFunctions() {
+        long functions = 0;
+        if (mHeatingCheck.isChecked()) functions |= Thermostat.Function.HEATING;
+        if (mCoolingCheck.isChecked()) functions |= Thermostat.Function.COOLING;
+        if (mOutingSettingCheck.isChecked()) functions |= Thermostat.Function.OUTING_SETTING;
+        if (mHotwaterOnlyCheck.isChecked()) functions |= Thermostat.Function.HOTWATER_ONLY;
+        if (mReservedModeCheck.isChecked()) functions |= Thermostat.Function.RESERVED_MODE;
+        device().setProperty(Thermostat.PROP_FUNCTION_STATES, Long.class,  functions);
+    }
+
+    private void setTemperature() {
+        final String editStr = mSettingTempEdit.getText().toString();
+        float reqTemp = PropertyValue.newValueObject(Float.class, editStr);
+        device().setProperty(Thermostat.PROP_SETTING_TEMPERATURE, Float.class, reqTemp);
+    }
 }
